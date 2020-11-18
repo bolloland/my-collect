373baf9d0f347b7b977ed6021ad27c561524cd10
ryan.bollettino@gmail.com <bollo@bollo-laptop.localdomain>
2020-11-17 23:32:39 -0600

    commit tree f4d3e2aac2bc99e95bf825f137b27a222b1a0344

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
 
 
+def my_collect(array)
+    
+    i = 0
+    collector = []
+        while i < array.length
+        collector << yield(array[i])
+        i += 1
+        end
+    collector
+end
