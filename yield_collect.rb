5863ffd81096ea5ce3ff9f2ce83c6bc4cba56935
ryan.bollettino@gmail.com <bollo@bollo-laptop.localdomain>
2020-11-17 23:36:01 -0600

    Done.

---
+++ yield_collect.rb
@@ -0,0 +1,10 @@
+### behaves like COLLECT METHOD
+def hello(array)
+    i = 0
+    collection = []
+    while i < array.length
+      collection << yield(array[i])
+      i += 1
+    end
+    collection
+  end
